302f929a40b4d149dd8d6e7a4551858be6d67d89
Nader Naguib <nadernaguibmoh@gmail.com>
2017-09-28 23:57:12 +0300

    fix login

--- Code/mariam-mousa-backend/mariam-mousa-backend/api/controllers/user.controller.js
+++ Code/mariam-mousa-backend/mariam-mousa-backend/api/controllers/user.controller.js
@@ -50,12 +50,17 @@ module.exports.login = function(req, res, next) {
                 console.log(err);
                 res.status(400).json(err);
             } else {
-                if (bcrypt.compareSync(password, user.password)) {
-                    console.log('user found' + user);
-                    var token = jwt.sign({ username: user.username }, 's3cr3t', { expiresIn: 3600 });
-                    res.status(200).json({ success: true, token: token });
+                if (user) {
+                    if (bcrypt.compareSync(password, user.password)) {
+                        console.log('user found' + user);
+                        var token = jwt.sign({ username: user.username }, 's3cr3t', { expiresIn: 3600 });
+                        res.status(200).json({ success: true, token: token });
+                    } else {
+                        console.log('user Unauthorized');
+                        res.status(401).json('Unauthorized');
+                    }
                 } else {
-                    console.log('user Unauthorized');
+                    console.log('User ' + username + ' not found!');
                     res.status(401).json('Unauthorized');
                 }
             }
