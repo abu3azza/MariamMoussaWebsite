2b6820c463e675bc08bf4bd62141d68b8d9ad3dc
Nader Naguib <nadernaguibmoh@gmail.com>
2017-10-17 20:51:40 +0200

    fix carousle homa page

--- Code/mariam-moussa-latest/js/app/controllers/MainController.js
+++ Code/mariam-moussa-latest/js/app/controllers/MainController.js
@@ -1,25 +1,120 @@
-app.controller('MainController', function($scope, $timeout) {
+app.controller('MainController', function($scope, $timeout, $sce) {
     $scope.bodyHtml = 'hwhwhwhwhwhwhwhwhwhw';
+    $scope.video = "https://www.youtube.com/embed/bJi9yceCmZ4";
+    $scope.trustedVideo = $sce.trustAsResourceUrl($scope.video);
+    // $scope.showModal = false;
+    // $scope.buttonClicked = "";
 
-    $scope.one = true; // setting the first div visible when the page loads
-    $scope.two = false; // hidden
-    $scope.three = false; // hidden
 
-    // Now have three functions that change the ng-show based on the click
-    $scope.showOne = function() {
-        $scope.one = true;
-        $scope.two = false;
-        $scope.three = false;
-    }
 
-    $scope.showTwo = function() {
-        $scope.one = false;
-        $scope.two = true; // now show this one
-        $scope.three = false;
-    }
+    $scope.toggleModal = function(btnClicked) {
+        $scope.buttonClicked = btnClicked;
+        $scope.showModal = !$scope.showModal;
+    };
+
+    $scope.open = function() {
+        alert('halsbala');
+        $scope.toggleModal('Edit Home Video');
+    };
+
+    $scope.submitForm = function(newvideo) {
+        $scope.video = newvideo;
+        $scope.showModal = !$scope.showModal;
+    };
+
+
+
+
+    $timeout(function() {
+        $('.panel-heading a[data-toggle="collapse"]').on('click', function() {
+            if ($(this).closest('.panel-heading').hasClass('active')) {
+                $(this).closest('.panel-heading').removeClass('active');
+            } else {
+                $('.panel-heading a[data-toggle="collapse"]').closest('.panel-heading').removeClass('active');
+                $(this).closest('.panel-heading').addClass('active');
+            }
+        });
+
+        /* Float Label Pattern Plugin for Bootstrap 3.1.0 by Travis Wilson
+         **************************************************/
+
+        (function($) {
+            $.fn.floatLabels = function(options) {
+
+                // Settings
+                var self = this;
+                var settings = $.extend({}, options);
+
+
+                // Event Handlers
+                function registerEventHandlers() {
+                    self.on('input keyup change', 'input, textarea', function() {
+                        actions.swapLabels(this);
+                    });
+                }
+
+
+                // Actions
+                var actions = {
+                    initialize: function() {
+                        self.each(function() {
+                            var $this = $(this);
+                            var $label = $this.children('label');
+                            var $field = $this.find('input,textarea').first();
+
+                            if ($this.children().first().is('label')) {
+                                $this.children().first().remove();
+                                $this.append($label);
+                            }
+
+                            var placeholderText = ($field.attr('placeholder') && $field.attr('placeholder') != $label.text()) ? $field.attr('placeholder') : $label.text();
+
+                            $label.data('placeholder-text', placeholderText);
+                            $label.data('original-text', $label.text());
+
+                            if ($field.val() == '') {
+                                $field.addClass('empty')
+                            }
+                        });
+                    },
+                    swapLabels: function(field) {
+                        var $field = $(field);
+                        var $label = $(field).siblings('label').first();
+                        var isEmpty = Boolean($field.val());
+
+                        if (isEmpty) {
+                            $field.removeClass('empty');
+                            $label.text($label.data('original-text'));
+                        } else {
+                            $field.addClass('empty');
+                            $label.text($label.data('placeholder-text'));
+                        }
+                    }
+                }
+
+
+                // Initialization
+                function init() {
+                    registerEventHandlers();
+
+                    actions.initialize();
+                    self.each(function() {
+                        actions.swapLabels($(this).find('input,textarea').first());
+                    });
+                }
+                init();
+
+
+                return this;
+            };
+
+            $(function() {
+                $('.float-label-control').floatLabels();
+            });
+        })(jQuery);
+    });
 
     function init() {
-
         $("#news-carousel").owlCarousel({
             // Most important owl features
             items: 2,
@@ -50,17 +145,4 @@ app.controller('MainController', function($scope, $timeout) {
         });
     }
     init();
-
-    $timeout(function() {
-        $('.panel-heading a[data-toggle="collapse"]').on('click', function() {
-            if ($(this).closest('.panel-heading').hasClass('active')) {
-                $(this).closest('.panel-heading').removeClass('active');
-            } else {
-                $('.panel-heading a[data-toggle="collapse"]').closest('.panel-heading').removeClass('active');
-                $(this).closest('.panel-heading').addClass('active');
-            }
-        });
-    });
-
-
 });
