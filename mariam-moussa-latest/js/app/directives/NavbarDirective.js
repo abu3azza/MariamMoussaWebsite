8fe3d76b7a832b7c975f91ad0e9450f65d3f10ee
abu3azza <mina.kahil@gmail.com>
2017-11-07 12:37:07 +0200

    3ala baraket ellah (Removing the extra irritating files)

--- mariam-moussa-latest/js/app/directives/NavbarDirective.js
+++
@@ -1,9 +0,0 @@
-angular.module('mmapp').directive('navbarDirective', navbarDirective);
-
-function navbarDirective() {
-    var vm = this;
-    return {
-        restrict: 'E',
-        templateUrl: 'js/app/directives/navbar.html'
-    };
-}
--- mariam-moussa-latest/mariam-moussa-latest/js/app/controllers/NavigationController.js
+++
@@ -1,8 +0,0 @@
-app.controller('NavigationController', ['$scope', '$location', function($scope, $location) {
-
-    $scope.getClass = function (path) {
-        return ($location.path().substr(0, path.length) === path) ? 'active' : '';
-    };
-
-}
-]);
