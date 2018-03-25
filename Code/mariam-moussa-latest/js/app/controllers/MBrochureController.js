0e759a1c72b81957c0dd9f883defdd2cf2329b8e
Mina_Personal\user1 <mina.kahil@asset.com.eg>
2018-03-25 21:04:38 +0200

    Adding Brochure Management

--- Code/mariam-moussa-latest/js/app/controllers/MBrochureController.js
+++ Code/mariam-moussa-latest/js/app/controllers/MBrochureController.js
@@ -1,11 +1,11 @@
 app.controller('MBrochureController', ['Upload', '$window', '$http', '$scope', function(Upload, $window, $http, $scope) {
-    alert("hi MBrochureController");
+    console.log("hi MBrochureController");
     var vm = this;
 
     vm.submit = function() { //function to call on form submit
 
-        // alert("hawhawhaw" + name);
-        // alert(JSON.stringify($scope.foundArticles.articles[i]));
+        // console.log("hawhawhaw" + name);
+        // console.log(JSON.stringify($scope.foundArticles.articles[i]));
 
         $http({
                 url: 'http://207.154.226.195:3000/api/deleteBrochure',
@@ -26,29 +26,29 @@ app.controller('MBrochureController', ['Upload', '$window', '$http', '$scope', f
 
     }
     vm.upload = function(file) {
-        // alert("File: =>" + JSON.stringify(file));
+        // console.log("File: =>" + JSON.stringify(file));
         Upload.upload({
             url: 'http://207.154.226.195:3000/api/uploadBrochure', //webAPI exposed to upload the file
             data: { file: file } //pass file as data, should be user ng-model
         }).then(function(resp) { //upload function returns a promise
             if (resp.data.error_code === 0) { //validate success
-                // $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                // $window.console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
 
-                $window.alert('Brouchure uploaded sucssesfuly');
+                $window.console.log('Brouchure uploaded sucssesfuly');
 
             } else {
-                $window.alert('an error occured');
+                $window.console.log('an error occured');
             }
         }, function(resp) { //catch error
             console.log('Error status: ' + resp.status);
-            $window.alert('Error status: ' + resp.status);
+            $window.console.log('Error status: ' + resp.status);
         }, function(evt) {
             console.log(evt);
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
             vm.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
         });
-        alert("finish upload ");
+        console.log("finish upload ");
     };
 
 }]);
